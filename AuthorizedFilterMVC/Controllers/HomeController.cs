b4a0f148ba8f918e4b9e9e80bc4f2ee7c6d3b96d
idrees04 <idreesar@yahoo.com>
2020-09-08 00:04:00 +0500

    Add project files.

---
+++ AuthorizedFilterMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthorizedFilterMVC.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [AllowAnonymous]
+        // [AllowAnonymous] filter mean everybody can access this  view this method will work with [Authorize] filter
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ AuthorizedFilterMVC/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using AuthorizedFilterMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AuthorizedFilterMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginDBEntities db = new LoginDBEntities();
+        // GET: Login
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Index(user u, string ReturnUrl)
+        {
+            if(IsValid(u)==true)
+            {
+                FormsAuthentication.SetAuthCookie(u.username, false);
+                Session["username"] = u.username.ToString();
+                if(ReturnUrl!=null)
+                {
+                    return Redirect(ReturnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public bool IsValid(user u)
+        {
+            var credentials = db.users.Where(model => model.username == u.username && model.password== u.password).FirstOrDefault();
+            if(credentials != null)
+            {
+                return (u.username == credentials.username && u.password == credentials.password);
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session["username"] = null;
+            return RedirectToAction("Index", "Home");          
+        }
+    }
+}
